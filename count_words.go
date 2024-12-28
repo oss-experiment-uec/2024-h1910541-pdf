266caa97b7f7195943f4a29bcb14540f6b181a31
ShinjiHikari <h1910541@gl.cc.uec.ac.jp>
2024-12-28 19:55:50 +0900

    count the words and return

---
+++ count_words.go
@@ -0,0 +1,13 @@
+package pdf
+
+import (
+	"regexp"
+)
+
+// CountWords counts the number of words in a given string
+func CountWords(text string) int {
+
+	re := regexp.MustCompile(`\b\w+\b`)
+	words := re.FindAllString(text, -1)
+	return len(words)
+}
